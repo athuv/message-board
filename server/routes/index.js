28ec3b4325a6b4c5d690c1f1027d8b62150da451
Ahamed A <21102277+athuv@users.noreply.github.com>
2023-11-26 13:28:57 +0530

    feat: add messages.json file to stor data

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,46 @@
 import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 var indexRouter = express.Router();
-/* GET home page. */
-indexRouter.get("/", function (req, res, next) {
-  const message = [
-    { name: "Kevin", message: "Hello World!!" },
-    { name: "John", message: "How are you?" },
-    { name: "Calvin", message: "Good" },
-  ];
 
-  res.json(message);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "./messages.json");
+
+indexRouter.get("/", function (req, res, next) {
+  fs.readFile(filePath, "utf8", function (err, data) {
+    if (err) {
+      console.error(err);
+    } else {
+      const jsonData = JSON.parse(data);
+      res.json(jsonData);
+    }
+  });
+});
+
+indexRouter.post("/", function (req, res, next) {
+  const formData = req.body;
+
+  fs.readFile(filePath, "utf8", function (err, data) {
+    if (err) {
+      console.error(err);
+    } else {
+      const existingData = JSON.parse(data);
+      const updatedData = [...existingData, formData];
+      const jsonData = JSON.stringify(updatedData, null, 2);
+
+      fs.writeFile(filePath, jsonData, "utf8", function (err) {
+        if (err) {
+          console.error(err);
+          res.status(500).send("Error adding data");
+        } else {
+          res.send("Message sent successfully");
+        }
+      });
+    }
+  });
 });
 
 export default indexRouter;
